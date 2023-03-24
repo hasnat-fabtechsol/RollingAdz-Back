219502dfd3c6004bc3a36b776b266bf2dc90872a
Hasnat <hasnat@fabtechsol.com>
2023-03-24 11:53:20 +0500

    userinfo added

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-  name: {
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
     type: String,
     required: true,
   },
